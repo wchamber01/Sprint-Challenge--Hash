ec1e2953b525bcb88a8f8d373b24ca101c8f1207
wchamber01 <williamchambers@gmail.com>
2020-05-08 17:32:30 -0400

    ex3 working

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,16 +1,25 @@
 def intersection(arrays):
 
+    # set a cache
     cache = {}
-    new_array = []
-    for item in arrays:
-        # print('i:', item)
-        for value in item:
-            if value not in cache:
-                cache[value] = 1
-                print('cache:', cache)
-            else:
-                cache[value] += 1
-        # return result
+    length = len(arrays)
+    # store for result
+    result = []
+    # loop through arrays to get all numbers
+    for numbers in arrays:
+        # loop through numbers to get each number
+        for item in numbers:
+            # if each number is not in the cache
+            if item not in cache:
+                # add number to cache as a key and give it a value of 1
+                cache[item] = 1
+            # else if number is in the cache
+            elif item in cache:
+                cache[item] += 1
+    for i in cache:
+        if cache[i] == length:
+            result.append(i)
+    return result
 
 
 if __name__ == "__main__":
@@ -19,5 +28,5 @@ if __name__ == "__main__":
     arrays.append(list(range(10, 20)) + [1, 2, 3])
     arrays.append(list(range(20, 30)) + [1, 2, 3])
     arrays.append(list(range(30, 40)) + [1, 2, 3])
-
+    # arrays.append(list(range(10, 12)) + [1, 2, 3, 4])
     print(intersection(arrays))
